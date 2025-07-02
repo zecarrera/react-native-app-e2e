4e034b0598f11f7a0f567ebc5c3a535ffbe51256
Jose Carrera <jose.carrera@amido.com>
2025-07-02 10:21:42 +0100

    tidy up

--- my-rn-app/e2e/login.spec.js
+++ my-rn-app/e2e/login.spec.js
@@ -2,19 +2,15 @@ const { expect } = require('chai');
 
 describe('Login Form', () => {
   it('should log in successfully with valid credentials', async () => {
-    // Wait for the Email input
     const emailInput = await $('~emailInput');
     await emailInput.setValue('test@example.com');
 
-    // Wait for the Password input
     const passwordInput = await $('~passwordInput');
     await passwordInput.setValue('12345');
 
-    // Tap the Login button
     const loginButton = await $('~loginButton');
     await loginButton.click();
 
-    // Wait for the success message
     const successMsg = await $('~successMessage');
     expect(await successMsg.isDisplayed()).to.be.true;
   });
--- my-rn-app/wdio.conf.js
+++ my-rn-app/wdio.conf.js
@@ -7,9 +7,7 @@ exports.config = {
     'appium:deviceName': 'Pixel_9a',
     'appium:platformVersion': '16',
     'appium:automationName': 'UiAutomator2',
-    // Update the path to your built APK or .app file
     'appium:app': 'android/app/build/outputs/apk/debug/app-debug.apk',
-    // 'appium:app': 'android/app/build/outputs/apk/release/app-release.apk',
     'appium:autoGrantPermissions': true,
   }],
   logLevel: 'info',
